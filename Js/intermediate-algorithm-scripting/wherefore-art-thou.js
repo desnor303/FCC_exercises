624bf310c88cdb7ad67115d5dffb4a2b89f5b3b5
desnor303 <desmonor@gmail.com>
2023-09-04 14:41:25 +0700

    change ".js"

--- Js/intermediate-algorithm-scripting/wherefore-art-thou.js
+++ Js/intermediate-algorithm-scripting/wherefore-art-thou.js
@@ -1,9 +1,15 @@
 function whatIsInAName(collection, source) {
-  let keySource = Object.keys(source);
-  return collection
-  .filter(ele =>keySource
-  .every(key => source[key] === ele[key])
-  )
+    let keySource = Object.keys(source);
+    return collection.filter((ele) =>
+        keySource.every((key) => source[key] === ele[key])
+    );
 }
 
-whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+whatIsInAName(
+    [
+        { first: "Romeo", last: "Montague" },
+        { first: "Mercutio", last: null },
+        { first: "Tybalt", last: "Capulet" },
+    ],
+    { last: "Capulet" }
+);
